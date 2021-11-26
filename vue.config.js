964f139172bdb382f7c13e5124eb874688dbe9c5
Pwb <pweibiao@163.com>
2021-11-26 15:04:05 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,18 @@ module.exports = {
   assetsDir: "static",
   configureWebpack: {
     devtool: "source-map"
+  },
+  devServer: {
+    port: 8888,
+    proxy: {
+      '/api':{
+        target: 'http://192.168.88.190:8090', // PWB
+        ws: false, // 是否代理websockets
+        changeOrigin: true, // 设置同源  默认false，是否需要改变原始主机头为目标URL
+        pathRewrite: {
+          '^/api': '' // 重写路径
+        }
+      }
+    }
   }
-  // devServer: {
-  //   proxy: {}
-  // }
 };
